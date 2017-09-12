7476c79331e4a161516803fd4728530cb4693a2e
João Sousa <jpd.sousa@campus.fct.unl.pt>
2017-09-12 08:21:37 +0100

    updated error pattern

--- src/basho_bench_driver_antidote_aql.erl
+++ src/basho_bench_driver_antidote_aql.erl
@@ -49,7 +49,7 @@ run(get, KeyGen, ValGen, #state{actor=Node} = State) ->
   case exec(Node, Query) of
     {ok, _} ->
       {ok, State};
-    {err, Reason} ->
+    {error, Reason} ->
       ?ERROR("Error in select query: ~p", [Reason]),
       {error, Reason, State}
   end;
@@ -66,7 +66,7 @@ run(put, KeyGen, ValGen, #state{actor=Node, artists=Artists, albums=Albums} = St
     {ok, _} ->
       {NewArtists, NewAlbums} = put_value(Table, Key, Artists, Albums),
       {ok, State#state{artists=NewArtists, albums=NewAlbums}};
-    {err, Err} ->
+    {error, Err} ->
       lager:error("Error in insert query: ~p", [Err]),
       {error, Err, State}
   end;
@@ -81,7 +81,7 @@ run(delete, KeyGen, ValGen, #state{actor=Node, artists=Artists, albums=Albums} =
     {ok, _} ->
       {NewArtists, NewAlbums} = del_value(Table, Key, Artists, Albums),
       {ok, State#state{artists=NewArtists, albums=NewAlbums}};
-    {err, Err} ->
+    {error, Err} ->
       lager:error("Error in delete query: ~p", [Err]),
       {error, Err, State}
   end;
@@ -92,7 +92,7 @@ run(get_all, _KeyGen, ValGen, #state{actor=Node} = State) ->
   case exec(Node, Query) of
     {ok, _} ->
       {ok, State};
-    {err, Err} ->
+    {error, Err} ->
       lager:error("Error in select all query: ~p", [Err]),
       {error, Err, State}
   end;
