ce46aec03790349825fc24ee884687babe3f8d16
João Sousa <jpd.sousa@campus.fct.unl.pt>
2017-09-26 15:23:14 +0100

    updated script

--- scripts/ssh_antidote_start.sh
+++ scripts/ssh_antidote_start.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-ANTIDOTE_INIT="cd antidote && make clear_background && exit"
+ANTIDOTE_INIT="screen -S backup -d -m ./init_antidote.sh"
 
 echo "Starting antidote instances"
-ssh -t jpdsousa@$1 $ANTIDOTE_INIT
-ssh -t jpdsousa@$2 $ANTIDOTE_INIT
-ssh -t jpdsousa@$3 $ANTIDOTE_INIT
+ssh jpdsousa@$1 $ANTIDOTE_INIT
+ssh jpdsousa@$2 $ANTIDOTE_INIT
+ssh jpdsousa@$3 $ANTIDOTE_INIT
 echo "Done"
