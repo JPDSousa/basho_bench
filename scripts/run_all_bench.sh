dd2774ad873a47e80ddb3280a97ec1a7c48458e7
João Sousa <jpd.sousa@campus.fct.unl.pt>
2017-09-26 14:09:29 +0100

    fixed echo

--- scripts/run_all_bench.sh
+++ scripts/run_all_bench.sh
@@ -1,32 +1,32 @@
 #!/usr/bin/env bash
 
-@echo "Resetting repo"
+echo "Resetting repo"
 git reset --hard
 git pull
 make all
 
-@echo "Writing ips to config files..."
+echo "Writing ips to config files..."
 sed -i "s/Nodes/[\"$1\", \"$2\", \"$3\"]/g" ./config/*.config
-@echo "Done"
+echo "Done"
 
-@echo "Fetching self public ip..."
+echo "Fetching self public ip..."
 MY_IP=$(curl v4.ifconfig.co)
 BASHO_NODE_NAME="basho@$MY_IP"
 sed -i "s/BASHOIP/$MY_IP/g" ./scripts/connect_dcs.escript
-@echo "Done"
+echo "Done"
 
-@echo "Writing ips to escript file"
+echo "Writing ips to escript file"
 sed -i "s/Nodes/[\"$1\", \"$2\", \"$3\"]/g" ./scripts/connect_dcs.escript
-@echo "Done"
+echo "Done"
 
-@echo "Preparing ssh connections"
+echo "Preparing ssh connections"
 ./scripts/ssh_setup.sh
-@echo "Done"
+echo "Done"
 
 for f in ./config/*.config
 do
 
-  @echo "Running $f"
+  echo "Running $f"
   ./basho_bench $f -N $BASHO_NODE_NAME -C antidote
   echo "[5] Cooling down..."
   sleep 1m
--- scripts/ssh_antidote_start.sh
+++ scripts/ssh_antidote_start.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-@echo "Starting antidote instances"
+echo "Starting antidote instances"
 ssh -t jpdsousa@$1 "cd antidote && make clear_background && bash"
 ssh -t jpdsousa@$2 "cd antidote && make clear_background && bash"
 ssh -t jpdsousa@$3 "cd antidote && make clear_background && bash"
-@echo "Done"
+echo "Done"
