d010f3b58cb046fd1ca88a8d8e2c2766650e11c8
João Sousa <jpd.sousa@campus.fct.unl.pt>
2017-09-26 15:27:07 +0100

    explicit strings

--- scripts/ssh_antidote_start.sh
+++ scripts/ssh_antidote_start.sh
@@ -1,9 +1,7 @@
 #!/usr/bin/env bash
 
-ANTIDOTE_INIT="screen -S backup -d -m ./init_antidote.sh"
-
 echo "Starting antidote instances"
-ssh jpdsousa@$1 $ANTIDOTE_INIT
-ssh jpdsousa@$2 $ANTIDOTE_INIT
-ssh jpdsousa@$3 $ANTIDOTE_INIT
+ssh jpdsousa@$1 "screen -S backup -d -m ./init_antidote.sh"
+ssh jpdsousa@$2 "screen -S backup -d -m ./init_antidote.sh"
+ssh jpdsousa@$3 "screen -S backup -d -m ./init_antidote.sh"
 echo "Done"
