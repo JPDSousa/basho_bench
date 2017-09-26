c3a5fc78eb72fd12743aed6577078ffad6f6dd47
João Sousa <jpd.sousa@campus.fct.unl.pt>
2017-09-26 14:49:03 +0100

    fixed antidote init

--- scripts/ssh_antidote_start.sh
+++ scripts/ssh_antidote_start.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
+ANTIDOTE_INIT="cd antidote && make clear_background && exit"
+
 echo "Starting antidote instances"
-ssh -t jpdsousa@$1 "cd antidote && make clear_background && bash"
-ssh -t jpdsousa@$2 "cd antidote && make clear_background && bash"
-ssh -t jpdsousa@$3 "cd antidote && make clear_background && bash"
+ssh -t jpdsousa@$1 $ANTIDOTE_INIT
+ssh -t jpdsousa@$2 $ANTIDOTE_INIT
+ssh -t jpdsousa@$3 $ANTIDOTE_INIT
 echo "Done"
