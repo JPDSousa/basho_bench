d01b89198d6910dd245d6f40772f28cce6c07e7f
João Sousa <jpd.sousa@campus.fct.unl.pt>
2017-10-05 08:49:34 +0100

    removed quotes

--- scripts/run_all_bench.sh
+++ scripts/run_all_bench.sh
@@ -23,7 +23,7 @@ echo "Preparing ssh connections"
 ./scripts/ssh_setup.sh $1 $2 $3
 echo "Done"
 
-for f in "${4:-./config/*.config}"
+for f in ${4:-./config/*.config}
 do
   ./scripts/ssh_antidote_start.sh $1 $2 $3
   echo "Running $f"
