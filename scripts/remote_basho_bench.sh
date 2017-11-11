dabebded7ba2e934eab3d130e44d33390c6606fe
João Sousa <jpd.sousa@campus.fct.unl.pt>
2017-11-11 12:12:04 +0000

    multiple fixs

--- scripts/remote_basho_bench.sh
+++ scripts/remote_basho_bench.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 cd basho_bench
-./basho_bench $1 -N basho2@$2 -C antidote
+./basho_bench $1 -N $2 -C antidote
--- scripts/run_all_bench.sh
+++ scripts/run_all_bench.sh
@@ -26,8 +26,8 @@ for f in ${6:-./config/**/*.config}
 do
   ./scripts/ssh_antidote_start.sh $1 $2 $3
   echo "Running $f"
-  ssh jpdsousa@$4 "screen -S basho -d -m ./basho_bench/scripts/remote_basho_bench $f $4"
-  ssh jpdsousa@$5 "screen -S basho -d -m ./basho_bench/scripts/remote_basho_bench $f $5"
+  ssh jpdsousa@$4 "screen -S basho -d -m ./basho_bench/scripts/remote_basho_bench $f basho2@$4"
+  ssh jpdsousa@$5 "screen -S basho -d -m ./basho_bench/scripts/remote_basho_bench $f basho3@$5"
   ./basho_bench $f -N $BASHO_NODE_NAME -C antidote
   ./scripts/ssh_antidote_kill.sh $1 $2 $3
   echo "[5] Cooling down..."
