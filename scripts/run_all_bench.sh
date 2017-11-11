4dd4fc5b208413b285bbf83e3941507813e5a5c9
João Sousa <jpd.sousa@campus.fct.unl.pt>
2017-11-11 10:18:36 +0000

    updated scripts

--- scripts/run_all_bench.sh
+++ scripts/run_all_bench.sh
@@ -1,9 +1,13 @@
 #!/usr/bin/env bash
 
-make all
+echo "Preparing ssh connections"
+./scripts/ssh_setup.sh $1 $2 $3 $4 $5
+echo "Done"
 
 echo "Writing ips to config files..."
-sed -i "s/Nodes/[\"$1\", \"$2\", \"$3\"]/g" ./config/**/*.config
+sed -i "s/Nodes/[\"$1\"]/g" ./config/**/*.config
+ssh jpdsousa@$4 'sed -i "s/Nodes/[\"$2\"]/g" ./basho_bench/config/**/*.config'
+ssh jpdsousa@$5 'sed -i "s/Nodes/[\"$3\"]/g" ./basho_bench/config/**/*.config'
 echo "Done"
 
 echo "Fetching self public ip..."
@@ -16,14 +20,12 @@ echo "Writing ips to escript file"
 sed -i "s/Nodes/[\"$1\", \"$2\", \"$3\"]/g" ./scripts/connect_dcs.escript
 echo "Done"
 
-echo "Preparing ssh connections"
-./scripts/ssh_setup.sh $1 $2 $3 $4 $5
-echo "Done"
-
 for f in ${6:-./config/*.config}
 do
   ./scripts/ssh_antidote_start.sh $1 $2 $3
   echo "Running $f"
+  ssh jpdsousa@$4 "screen -S basho -d -m ./basho_bench $f -N basho2@$4 -C antidote"
+  ssh jpdsousa@$5 "screen -S basho -d -m ./basho_bench $f -N basho3@$5 -C antidote"
   ./basho_bench $f -N $BASHO_NODE_NAME -C antidote
   ./scripts/ssh_antidote_kill.sh $1 $2 $3
   echo "[5] Cooling down..."
