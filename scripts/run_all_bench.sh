1a4ec3ca1925a39e8f5f721f60f2db31e716835a
João Sousa <jpd.sousa@campus.fct.unl.pt>
2017-11-16 14:47:02 +0000

    init daemon before running basho bench

--- scripts/run_all_bench.sh
+++ scripts/run_all_bench.sh
@@ -26,8 +26,8 @@ for f in ${6:-./config/**/*.config}
 do
   ./scripts/ssh_antidote_start.sh $1 $2 $3
   echo "Running $f"
-  ssh jpdsousa@$4 "cd basho_bench && screen -S basho -d -m ./basho_bench $f -N basho@$4 -C antidote"
-  ssh jpdsousa@$5 "cd basho_bench && screen -S basho -d -m ./basho_bench $f -N basho@$5 -C antidote"
+  ssh jpdsousa@$4 "epmd -daemon && cd basho_bench && screen -S basho -d -m ./basho_bench $f -N basho@$4 -C antidote"
+  ssh jpdsousa@$5 "epmd -daemon && cd basho_bench && screen -S basho -d -m ./basho_bench $f -N basho@$5 -C antidote"
   ./basho_bench $f -N $BASHO_NODE_NAME -C antidote
   ./scripts/ssh_antidote_kill.sh $1 $2 $3
   echo "[5] Cooling down..."
